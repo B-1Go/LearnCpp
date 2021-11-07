4c8093633f07c877f7b40846bec25d5edf18b8b5
B-1GO <mj2000go@naver.com>
2021-11-07 23:04:56 +0900

    day010_04

--- LearnAboutCpp/main3.cpp
+++ LearnAboutCpp/main3.cpp
@@ -17,11 +17,56 @@ int Factorial(int _iNum)
 // 가독성, 구현의 용이
 // 탈출조건필수, 함수를 변수처럼 사용 & 함수 호출 및 해제 비용 으로 성능저하
 
+// 재귀함수 팩토리얼
+int Factorial_Re(int _iNum)
+{
+	if (_iNum < 2)
+	{
+		return 1;
+	}
+	return _iNum * Factorial_Re(_iNum - 1);
+}
+
+// 피보나치 수열
+// 1 1 2 3 5 8 13 21 34 55....
+int Fibonacci(int _iNum)
+{
+	if (1 == _iNum || 2 == _iNum)
+	{
+		return 1;
+	}
+	int iPrev1 = 1;
+	int iPrev2 = 1;
+	int iValue;
+	for (int j = 1;j < _iNum - 1; ++j)
+	{
+		iValue = iPrev1 + iPrev2;
+		iPrev1 = iPrev2;
+		iPrev2 = iValue;
+	}
+	return iValue;
+}
+
+// 피보나치 수열 재귀함수
+// 굉장히 느리기 때문에 "꼬리재귀" 같은 해결 방법이 있다고 한다.[심화]
+int Fibonacci_Re(int _iNum)
+{
+	if (1 == _iNum || 2 == _iNum)
+	{
+		return 1;
+	}
+	return Fibonacci_Re(_iNum - 1) + Fibonacci_Re(_iNum - 2);
+}
 
 int main()
 {
 	int iValue = Factorial(4);
 	iValue = Factorial(10);
+	iValue = Factorial_Re(10);
+
+	iValue = Fibonacci(10);
+
+	iValue = Fibonacci_Re(3);
 
 	return 0;
 }
