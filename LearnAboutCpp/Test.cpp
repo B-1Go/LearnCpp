cfc1ee119a56a979edc8b2cf0176f4885e2f3c6b
B-1Go <mj2000go@naver.com>
2021-11-08 23:35:46 +0900

    day011_05

---
+++ LearnAboutCpp/Test.cpp
@@ -0,0 +1,3 @@
+#include "common.h"
+
+int g_iExtern = 0;
--- LearnAboutCpp/func.cpp
+++ LearnAboutCpp/func.cpp
@@ -1,8 +1,13 @@
 #include "func.h"
+#include "common.h"
+#include <stdio.h>
 
+// static int g_iStatic = 0;
 
 int Add(int a, int b)
 {
+	printf("g_iSatic °ª : %d\n", g_iStatic);
+	printf("g_iExtern °ª : %d\n", g_iExtern);
 	return a + b;
 }
 
