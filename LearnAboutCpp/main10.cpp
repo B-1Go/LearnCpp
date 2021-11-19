0e99af1ef6aadff56e1376d912d1e37dfd3914c0
B-1Go <mj2000go@naver.com>
2021-11-19 12:53:58 +0900

    day020_01

--- LearnAboutCpp/main10.cpp
+++ LearnAboutCpp/main10.cpp
@@ -2,11 +2,26 @@
 #include <stdio.h>
 #include <wchar.h>
 
+#include <assert.h>
+
 // 메모리 영역
 // 1. 스택
 // 2. 데이터
 // 3. 힙
 // 4. ROM(코드)
+
+// 함수의 오버로딩
+// 동일한 함수명 이지만, 인자값의 자료형이나 갯수를 다르게 해서 사용할 수 있다.
+// 주의: 함수의 오버라이딩(나중에 헷갈리는 단골 손님)
+void Test(int a)
+{
+
+}
+void Test(float a)
+{
+
+}
+
 
 unsigned int GetLength(const wchar_t* _pStr)
 {
@@ -25,6 +40,20 @@ unsigned int GetLength(const wchar_t* _pStr)
 	return i;
 }
 
+void StrCat(wchar_t* _pDest, unsigned int _iBufferSize, const wchar_t* _pSrc)
+{
+	int iDestLen = GetLength(_pDest);
+	int iSrcLen = GetLength(_pSrc);
+
+	if (_iBufferSize < iDestLen + iSrcLen + 1)
+	{
+		assert(nullptr);
+	}
+
+	// 문자열 이어 붙이기
+
+}
+
 int main()
 {
 	wchar_t szName[10] = L"Raimond";
@@ -32,8 +61,16 @@ int main()
 	// int iLen = wcslen(szName);
 	int iLen = GetLength(szName);
 
+	// 함수의 오버로딩
+	Test(10);
+	Test(3.14f);
+
+
 	// 문자열 이어 붙이기
-	// wcscat_s();
+	wchar_t szString[100] = L"abc";
+
+
+	wcscat_s(szString, 100, L"def");
 
 	return 0;
 }
