74351ec569c18c033dfff6065b94253bef1f0350
B-1Go <mj2000go@naver.com>
2021-12-22 19:34:39 +0900

    day053_01

---
+++ LearnAboutCpp/main16.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "CList.h"
+
+
+int main()
+{
+	CList<float> list;
+
+	for (int i = 0; i < 4; i++)
+	{
+		list.push_back(i);
+	}
+
+
+	CList<int> ilist;
+
+	for (int i = 0; i < 4; i++)
+	{
+		ilist.push_back(i);
+	}
+
+	// C printf
+	std::cout << "¾È³ç" << 10 << std::endl;
+
+
+	// C scanf
+	int iInput = 0;
+	std::cin >> iInput;
+
+	return 0;
+}
